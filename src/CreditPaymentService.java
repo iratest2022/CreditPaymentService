8438ced9bc0e63f16e3c9612a098d14727841bc4
Irina.Test <ira.test2022@gmail.com>
2023-03-28 08:45:57 +0300

    made changes

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class CreditPaymentService {
+public class CreditPaymentService {
+    public double calculate (int credit, double creditRate, int duration){
+        // Месячная кредитная ставка
+        double crm = creditRate/100/12;
+
+        // Расчёт срока кредита из лет в месяцы
+        int dm = duration * 12;
+
+        // Возведение в степень для промежуточного расчёта
+        double elevate = Math.pow(1+crm, dm);
+
+        // Расчитываем ежемесячный платёж
+        double creditPayment = credit * (crm + (crm / (elevate - 1)));
+
+        // Возвращаем сумму месячного платежа
+        return creditPayment;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CreditPaymentService service = new CreditPaymentService();
+        // Сумма кредита в рублях
+        int credit = 1_000_000;
+
+        // Годовая процентная ставка
+        double creditRate = 9.99;
+
+        // Срок кредита в годах
+        int duration = 3;
+
+        // Считаем ежемесячный аннуитентный платёж
+        double creditPayment = service.calculate(credit, creditRate, duration);
+
+        // округляем до целого числа
+        String creditPaymentInteger = String.format("%.0f",creditPayment);
+        System.out.println("Сумма кредита: " + credit + " руб.");
+        System.out.println("Годовая процентная ставка: " + creditRate + " %.");
+        System.out.println("Срок кредита: " + duration + " лет.");
+        System.out.println("Сумма ежемесячного платежа: " + creditPaymentInteger + " руб.");
     }
 }
